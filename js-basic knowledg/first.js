7273d1869ab5d3aa8b6010e6944784d20a52af1c
sakhawathussain01 <sakhawathussayn760@gmail.com>
2024-01-07 10:16:09 +0500

    updated

--- js-basic knowledg/first.js
+++ js-basic knowledg/first.js
@@ -1,63 +1,63 @@
-//------------------- firt  day 17---------
+//------------------- firt  day ---------
 // variables and console
-// let fulName="Sakhavat Hossain";//stirng
-// let age = 20;//num
-// let price = 100;//num
-// let x = null;//null
-// let y = undefined;//undefind
-// let isfollow = true;//boolien
-// console.log(fulName);
-// ocnsole.log(age);
-// console.log(price);
-// console.log(x)
-// console.log(y)
-// console.log(isfollow)
+let fulName="Sakhavat Hossain";//stirng
+let age = 20;//num
+let price = 100;//num
+let x = null;//null
+let y = undefined;//undefind
+let isfollow = true;//boolien
+console.log(fulName);
+ocnsole.log(age);
+console.log(price);
+console.log(x)
+console.log(y)
+console.log(isfollow)
 
 
 //--------objects------------//
-// const student = {
-//     fullName: 'Sakhavat Hossain',
-//     age:20,
-//     gpa:4.3,
-//     isPass:true,
-// };
-// console.log(student)
-// console.log(student["isPass"])
-// console.log(student.isPass)
-// student["age"] = student["age"] + 1;
-// console.log(student["gpa"]);
+const student = {
+    fullName: 'Sakhavat Hossain',
+    age:20,
+    gpa:4.3,
+    isPass:true,
+};
+console.log(student)
+console.log(student["isPass"])
+console.log(student.isPass)
+student["age"] = student["age"] + 1;
+console.log(student["gpa"]);
 ////------------practice section-------
-// const profile = {
-//     fullName:"Sakhavat_hossain",
-//     followers: 340,
-//     following: 20,
-//     isFollow: false,
-// };
-// console.log(profile);
+const profile = {
+    fullName:"Sakhavat_hossain",
+    followers: 340,
+    following: 20,
+    isFollow: false,
+};
+console.log(profile);
 
 //------------------ 2nd day ----------
 
 //---------------------arthematic operators-------
 
-// let a = 5;
-// let b = 2;
-// console.log("a = ", a,"& b = ",b);
-// console.log("a + b =", a + b);
-// console.log("a - b =", a - b);
-// console.log("a * b =", a * b);
-// console.log("a / b =", a / b);
-// console.log("a % b =", a % b);        //reminder
-// console.log("a ** b =", a ** b);     //exponant
+let a = 5;
+let b = 2;
+console.log("a = ", a,"& b = ",b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);        //reminder
+console.log("a ** b =", a ** b);     //exponant
 
 
 // --------------------comparision operators--------
 
 // let a = 5;
 // let b = "5";
-// console.log("a == b", a==b);  //equal to
-// console.log("a != b", a!=b);  //not equal to
-// console.log("a === b", a===b);  //equal to and type
-// console.log("a !== b", a!==b);   //not equal to and type
+console.log("a == b", a==b);  //equal to
+console.log("a != b", a!=b);  //not equal to
+console.log("a === b", a===b);  //equal to and type
+console.log("a !== b", a!==b);   //not equal to and type
 
 // ----------------------logical operators----------
 
@@ -65,8 +65,8 @@
 // let b = 5;
 // ------------And operator--------
 //---only true if both two conditions are true----
-// let cond1 = a > b;
-// let cond2 = a === 6;      
+let cond1 = a > b;
+let cond2 = a === 6;      
 // console.log("cond1 && cond2=",cond1 && cond2); //true
 
 
@@ -77,7 +77,7 @@
 
 // let cond1 = a > b;
 // let cond2 = a !=6;
-// console.log("cond1 || cond2 =",cond1 || cond2) //true
+console.log("cond1 || cond2 =",cond1 || cond2) //true
 
 //----------Not  operator---------
 //---  if true then gives false, if  false then gives true----
@@ -86,7 +86,7 @@
 
 // let cond1 = a > b;
 // let cond2 = a !=6;
-// console.log("!(6 >5) =", !(6 > 5)) //false
+console.log("!(6 >5) =", !(6 > 5)) //false
 
 
 // ---------------Conditional statement------
@@ -106,17 +106,17 @@
 
 // -----------------------example 2-----
 
-// let mode = "light";
-// let color;
+let mode = "light";
+let color;
 
 
-// if(mode === "dark"){
-//     color = "black"
-// }
-// if(mode === "light"){
-//     color = "white"
-// }
-// console.log (color);
+if(mode === "dark"){
+    color = "black"
+}
+if(mode === "light"){
+    color = "white"
+}
+console.log (color);
 
 // 1*------------if else and else statement-------
 
@@ -135,37 +135,37 @@
 
 //--------------------- pratice section----------
 
-// let num = prompt("please enter number")
+let num = prompt("please enter number")
 
-// if(num % 5===0){
-//     console.log(num,"is multiple of 5");
-// }
-// else if(num % 2===0){
-//     console.log(num,"is multiple of 2")
-// }
-// else if(num % 3===0){
-//     console.log(num,"is multiple of 3")
-// }
-// else{
-//     console.log(num,"is not multiple of 5");
-// }
-// console.log("hello world");
+if(num % 5===0){
+    console.log(num,"is multiple of 5");
+}
+else if(num % 2===0){
+    console.log(num,"is multiple of 2")
+}
+else if(num % 3===0){
+    console.log(num,"is multiple of 3")
+}
+else{
+    console.log(num,"is not multiple of 5");
+}
+console.log("hello world");
 
 //------------------ practice 2 if and else if-------
-// let score = prompt("enter your score(0-100)");
-// let grade;
+let score = prompt("enter your score(0-100)");
+let grade;
 
-// if(score >= 90 && score <= 100){
-//     grade = "A";
-// } else if(score >= 70 && score <= 89){
-//     grade = "B";
-// } else if(score >= 60 && score <= 69){
-//     grade = "C";
-// } else if(score >= 50 && score <= 59){
-//     grade = "D";
-// } else if(score >= 0 && score <= 49){
-//     grade = "F";
-// }
+if(score >= 90 && score <= 100){
+    grade = "A";
+} else if(score >= 70 && score <= 89){
+    grade = "B";
+} else if(score >= 60 && score <= 69){
+    grade = "C";
+} else if(score >= 50 && score <= 59){
+    grade = "D";
+} else if(score >= 0 && score <= 49){
+    grade = "F";
+}
 // console.log("your grade is",grade);
 
 //-------------------- 3rd day 19 nov-------
@@ -173,21 +173,21 @@
 //---------------------- loops--------------
 
 //--------------- for loop--------------
-// for (let cont = 1; cont <= 100; cont++){
-//     console.log("sakhavat husssain");
-// }
-// let sum = 0;
-// let n = 1;
-// for (i = 1 ;i <=100; i++){
-//     n = n + i; 
-// }
+for (let cont = 1; cont <= 100; cont++){
+    console.log("sakhavat husssain");
+}
+let sum = 0;
+let n = 1;
+for (i = 1 ;i <=100; i++){
+    n = n + i; 
+}
 // console.log(n);
 //----------------- while loop--------------
-// let i = 1;
-// while(i <= 10){
-//     console.log("Apna Colledge");
-//     i++;
-// }
+let i = 1;
+while(i <= 10){
+    console.log("Apna Colledge");
+    i++;
+}
 //-------------------- do while loop----------
 // let i = 1;
 // do{
@@ -198,27 +198,27 @@
 
 //--------------------- practice portion----------
 //-----------------q1--------------------------------
-// for (let num = 0; num <=100; num++){
-//     if(num % 2 == 0){
-// console.log("num = ", num);
-//     }
-// }
+for (let num = 0; num <=100; num++){
+    if(num % 2 == 0){
+console.log("num = ", num);
+    }
+}
 //---------------------q2---------------------------
-// let gamenum = 25;
+let gamenum = 25;
 
-// let usernum = prompt("enter your number :");
-// while(usernum != gamenum){
-//    usernum = prompt("enter your number again :");
-// }
-// console.log("congratulation you enter right number.");
+let usernum = prompt("enter your number :");
+while(usernum != gamenum){
+   usernum = prompt("enter your number again :");
+}
+console.log("congratulation you enter right number.");
 
 
 //---------------------- Day 3--------------
 //-------------------- arrays----------
-// let marks = [95 ,82, 73, 62, 36];
-// console.log(marks);
-// let names = ["sakhavat", "waqar"];
-// console.log(names);
+let marks = [95 ,82, 73, 62, 36];
+console.log(marks);
+let names = ["sakhavat", "waqar"];
+console.log(names);
 //-----------practice section----------
 // let fulName = prompt('enter your full name without spaces');
 
@@ -227,7 +227,7 @@
 
 //----------------------------Day 4 -------------
 //-----------------Arrays in Js-------------
-// let cities = ['lahore','islamabad','karachi','gilgit' ]
+let cities = ['lahore','islamabad','karachi','gilgit' ]
 
 // for(let city of cities){
 //     console.log(city.toUpperCase());
